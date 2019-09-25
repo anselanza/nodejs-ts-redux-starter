15307972e74f7908b32211cd3ea277f391ea1eb0
Stephen Buchanan <stephen@pop-os.localdomain>
2019-09-25 14:08:24 +0200

    ignore dist folder in repo

--- dist/store/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reducers_1 = require("./system/reducers");
-const redux_1 = require("redux");
-const rootReducer = redux_1.combineReducers({
-    system: reducers_1.systemReducer
-});
-exports.default = rootReducer;
-//# sourceMappingURL=index.js.map
--- dist/store/system/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ADD_CLIENT = 'ADD_CLIENT';
-exports.DELETE_CLIENT = 'DELETE_CLIENT';
-//# sourceMappingURL=types.js.map
